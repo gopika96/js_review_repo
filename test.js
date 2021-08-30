e4a8dcabcc9bd5e87cfbe926bcdf0756b130eb32
Gopika Bhardwaj <gbhard3@uic.edu>
2021-08-30 13:48:05 -0500

    adding daysOfWeek variable to test.js

--- test.js
+++ test.js
@@ -5,8 +5,9 @@ function cwebTestFunction(){
     var name = "C Fulton";
     var subject = "Web Tooling Class";
     var meetingTime;
+    var daysOfWeek = "M,W,F"
 
     meetingTime = "4pm";
 
-    console.log('Name:${name} Subject: ${subject} Meeting Time: ${meetingTime}');
+    console.log('Name:${name} Subject: ${subject} Meeting Time: ${meetingTime} Meeting Days: ${daysOfWeek}');
 }
