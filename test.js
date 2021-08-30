ff3caae49e561cc324b88a934f11d7c05324db29
Gopika Bhardwaj <gbhard3@uic.edu>
2021-08-30 13:16:01 -0500

    Initial commit

---
+++ test.js
@@ -0,0 +1,12 @@
+cwebTestFunction();
+
+//define
+function cwebTestFunction(){
+    var name = "CF";
+    var subject = "Web Tooling";
+    var meetingTime;
+
+    meetingTime = "2pm";
+
+    console.log('Name:${name} Subject: ${subject} Meeting Time: ${meetingTime}');
+}
