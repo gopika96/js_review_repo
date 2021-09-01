0e6a0c59b81903dd8ee088e7b16e11506813c0c7
Gopika Bhardwaj <gbhard3@uic.edu>
2021-09-01 07:46:44 -0500

    Adding variable and console output to test.js

--- test.js
+++ test.js
@@ -4,10 +4,12 @@ cwebTestFunction();
 function cwebTestFunction(){
     var name = "C Fulton";
     var subject = "Web Tooling Class";
-    var meetingTime;
+    var meetingTime, semesterLength;
     var daysOfWeek = "M,W,F"
 
     meetingTime = "4pm";
+    semesterLength = "16"
 
     console.log('Name:${name} Subject: ${subject} Meeting Time: ${meetingTime} Meeting Days: ${daysOfWeek}');
+    console.log('Semester Length is ${semesterLength}');
 }
